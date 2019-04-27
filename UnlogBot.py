d580e4e468cd6b3fbf940268d11f36938d41a7fb
ahfontaine <axcellhf@gmail.com>
2019-04-27 13:59:18 -0400

    Fixed Slap

--- UnlogBot.py
+++ UnlogBot.py
@@ -6,21 +6,20 @@ TOKEN = 'NTcxNDgyNTMyMjY5MzkxOTEz.XMOeJg.5sOWsJt0MTbuiEsIRkrIE5AVsXs'
 BOT_PREFIX = ('+','!')
 
 client = Bot(command_prefix=BOT_PREFIX)
-
-bot.command(pass_context=True)
-async def slap(member: discord.Member):
+@client.command()
+async def slap(ctx, member: discord.Member):
         """<member>: Be careful with this one."""
-        await bot.say("{} slaps {} around a bit with a large trout.".format(ctx.message.author.mention, member.mention))
+        await ctx.send("{} slaps {} around a bit with a large trout.".format(ctx.message.author.mention, member.mention))
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+#@client.event
+#async def on_message(message):
+#    # we do not want the bot to reply to itself
+#    if message.author == client.user:
+#        return
+#
+#    if message.content.startswith('!hello'):
+#        msg = 'Hello {0.author.mention}'.format(message)
+#        await message.channel.send(msg)
 
 @client.event
 async def on_ready():
